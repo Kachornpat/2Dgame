575192ae78e452d68d96bfc715abd14b9d9009ff
kachornpat <kchornpg@gmail.com>
2023-12-18 23:10:38 +0700

    draw rect

--- My2DGame/src/main/GamePanel.java
+++ My2DGame/src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -37,7 +39,24 @@ public class GamePanel extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		
+		update();
 		
+		repaint();
+		
+	}
+	
+	public void update()
+	{
+		
+	}
+	public void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		g2.fillRect(100, 100, tileSize, tileSize);
+		g2.dispose();
 	}
 }
 
